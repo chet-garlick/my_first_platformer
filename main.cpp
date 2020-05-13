e847489d790ee63f70d42464f8d84a28ebadc507
Chet Garlick <chet.garlick@aggiemail.usu.edu>
2020-05-13 03:46:08 -0600

    Added basic Platform class which is a wrapper for SFML's RectangleShape class where developers can add more functionality. Platform's draw correctly in window.

--- main.cpp
+++ main.cpp
@@ -8,16 +8,14 @@
 
 //Project header files
 #include "Player.h"
+#include "Platform.h"
+
 
 int main()
 {
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-    std::string textureFile = "resources/ninja-black-32x32.png";
-
-    // Create player object
-    Player player(textureFile, 32);
-
+    
     sf::Vector2i centerWindow((sf::VideoMode::getDesktopMode().width / 2 - 445) , (sf::VideoMode::getDesktopMode().height / 2) - 480);
   
     window.create(sf::VideoMode(1000,600), "My Platformer", sf::Style::Titlebar | sf::Style::Close);
@@ -27,11 +25,23 @@ int main()
     window.setKeyRepeatEnabled(true);
     
     window.setFramerateLimit(60);
+    
+    
+
+    // Create player object
+    std::string textureFile = "resources/ninja-black-32x32.png";
+    Player player(textureFile, 32);
 
     //draw background (let's start with white, to see our sprite better)
     //
     sf::RectangleShape background(sf::Vector2f(window.getSize().x,window.getSize().y));
     background.setFillColor(sf::Color::White);
+
+    // Create a floor platform object as a test
+    // TODO:: figure out why this isnt drawing correctly.
+    //Platform floor(0, window.getSize().y, window.getSize().x,100, sf::Color::Blue);
+
+    Platform floor(0, window.getSize().y-10, window.getSize().x,10, sf::Color::Black);
 
     // Start the game loop
     while (window.isOpen())
@@ -92,12 +102,25 @@ int main()
         // Clear screen
         window.clear();
 
+        
+        
         window.draw(background);
         
+
+        //Draw the floor
+        floor.draw(window);
+
         // Draw the player
         player.draw(window);
+        
+        
+        
+        
         // Update the window
         window.display();
+
+
+
     }
     return 0;
 }
